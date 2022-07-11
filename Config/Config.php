7c76f7457225c0ca11826906cef03802a4ef314d
erik-gh <eriktecse@gmail.com>
2022-07-11 17:09:23 -0500

    cambio de config para levantar en local

--- Config/Config.php
+++ Config/Config.php
@@ -1,7 +1,7 @@
 <?php 
 
-	//define('BASE_URL', 'http://localhost/kardex/');
-	const BASE_URL = 'http://10.50.20.132/kardex';
+	define('BASE_URL', 'http://localhost/kardex');
+	// const BASE_URL = 'http://10.50.20.132/kardex';
 	// const BASE_URL = 'http://192.168.1.2/kardex';
 
 	// ZONA HORARIA
@@ -11,9 +11,9 @@
 	const DB_HOST 		= "localhost";
 	const DB_NAME 		= "sistema_ka";
 	const DB_USER 		= "root";
-	const DB_PASSWORD 	= ".@dmin#Root-2020.";
+	//const DB_PASSWORD 	= ".@dmin#Root-2020.";
+	const DB_PASSWORD 	= "";
 	const DB_CHARSET 	= "charset=utf8";
-
 
 	
 	//DELIMITADOR DECIMAL Y MILES EJ 24,500.77
